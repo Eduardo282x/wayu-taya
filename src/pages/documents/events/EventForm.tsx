7ede1bc49055c265894831008d8a4b84b8ceb5b4
Eduardo282x <eduardokunmixyeah@gmail.com>
2025-05-10 10:10:22 -0400

    add eventForm and router

---
+++ src/pages/documents/events/EventForm.tsx
@@ -0,0 +1,14 @@
+import { Button } from '@/components/ui/button'
+import React from 'react'
+import { useNavigate } from 'react-router'
+
+export const EventForm = () => {
+    const navigate = useNavigate()
+    return (
+        <div>EventForm
+
+            <Button onClick={() => navigate(-1)}>Volver</Button>
+
+        </div>
+    )
+}
--- src/pages/documents/events/Events.tsx
+++ src/pages/documents/events/Events.tsx
@@ -1,10 +1,15 @@
 import { TableComponents } from '@/components/table/TableComponents'
 import { columnEvents, dataEvents } from './events.data'
+import { useNavigate } from 'react-router'
+import { Button } from '@/components/ui/button';
 
 export const Events = () => {
+  const navigate = useNavigate();
   return (
     <div>Eventos
 
+
+<Button onClick={() => navigate('formulario')}>Agregar evento</Button>
         <TableComponents column={columnEvents} data={dataEvents}/>
     </div>
   )
