cdf5c73e59025f4de5d0d4b0ecbb168d82b0baa1
Lee-gif212 <leefereira2@gmail.com>
2025-05-24 02:26:02 -0400

    Realice la vista de los eventos

--- src/pages/documents/events/Events.tsx
+++ src/pages/documents/events/Events.tsx
@@ -1,18 +1,94 @@
-import { TableComponents } from '@/components/table/TableComponents'
-import { columnEvents, dataEvents } from './events.data'
 import { useNavigate } from 'react-router'
 import { Button } from '@/components/ui/button';
 import { HeaderPages } from '@/pages/layout/Header';
 import { FaRegCalendarAlt } from 'react-icons/fa';
-
+import { FaArrowCircleDown } from "react-icons/fa";
+import { eventos } from './events.data';
 export const Events = () => {
   const navigate = useNavigate();
+
   return (
     <div>
       <HeaderPages title='Eventos' Icon={FaRegCalendarAlt} />
 
       <Button onClick={() => navigate('formulario')} className='mb-2'>Agregar evento</Button>
-      <TableComponents column={columnEvents} data={dataEvents} />
+
+      {/* <TableComponents column={columnEvents} data={dataEvents} /> */}
+
+      <div className='spaceEvents'>
+        {eventos && eventos.map(eve => (
+        <div className='flex items-center justify-between w-full p-4 rounded-4xl bg-blue-500'>
+        <div className='flex gap-2'>
+          <div className='bg-white rounded-2xl p-1 text-center w-20 h-20'>
+            <p className='bg-blue-500 text-white rounded-full px-2'>{eve.mes}</p>
+            <p className='text-2xl text-blue-800 font-bold'>{eve.fecha}</p>
+            <p>{eve.dia}</p>
+          </div>
+          <div className='flex flex-col justify-between'>
+            <p className='text-white text-xl'>{eve.nombre}</p>
+            <p className='bg-blue-200 text-blue-500 py-2 px-3 rounded-full font-bold'>{eve.mes} {eve.fecha} a las {eve.hora}</p>
+          </div>
+        </div>
+        <FaArrowCircleDown className='text-white text-3xl' />
+        </div>
+      ))}
+        <div className='flex items-center justify-between w-full p-4 rounded-4xl bg-blue-500'>
+        <div className='flex gap-2'>
+          <div className='bg-white rounded-2xl p-1 text-center w-20 h-20'>
+            <p className='bg-blue-500 text-white rounded-full px-2'>Feb</p>
+            <p className='text-2xl text-blue-800 font-bold'>21</p>
+            <p>Sab</p>
+          </div>
+          <div className='flex flex-col justify-between'>
+            <p className='text-white text-xl'>Gala Benéfica</p>
+            <p className='bg-blue-200 text-blue-500 py-2 px-3 rounded-full font-bold'>Feb 21 a las 8:00am - 11:00am</p>
+          </div>
+        </div>
+        <FaArrowCircleDown className='text-white text-3xl' />
+        </div>
+        <div className='flex items-center justify-between w-full p-4 rounded-4xl bg-blue-500'>
+        <div className='flex gap-2'>
+          <div className='bg-white rounded-2xl p-1 text-center w-20 h-20'>
+            <p className='bg-blue-500 text-white rounded-full px-2'>Feb</p>
+            <p className='text-2xl text-blue-800 font-bold'>21</p>
+            <p>Sab</p>
+          </div>
+          <div className='flex flex-col justify-between'>
+            <p className='text-white text-xl'>Gala Benéfica</p>
+            <p className='bg-blue-200 text-blue-500 py-2 px-3 rounded-full font-bold'>Feb 21 a las 8:00am - 11:00am</p>
+          </div>
+        </div>
+        <FaArrowCircleDown className='text-white text-3xl' />
+        </div>
+        <div className='flex items-center justify-between w-full p-4 rounded-4xl bg-blue-500'>
+        <div className='flex gap-2'>
+          <div className='bg-white rounded-2xl p-1 text-center w-20 h-20'>
+            <p className='bg-blue-500 text-white rounded-full px-2'>Feb</p>
+            <p className='text-2xl text-blue-800 font-bold'>21</p>
+            <p>Sab</p>
+          </div>
+          <div className='flex flex-col justify-between'>
+            <p className='text-white text-xl'>Gala Benéfica</p>
+            <p className='bg-blue-200 text-blue-500 py-2 px-3 rounded-full font-bold'>Feb 21 a las 8:00am - 11:00am</p>
+          </div>
+        </div>
+        <FaArrowCircleDown className='text-white text-3xl' />
+        </div>
+        <div className='flex items-center justify-between w-full p-4 rounded-4xl bg-blue-500'>
+        <div className='flex gap-2'>
+          <div className='bg-white rounded-2xl p-1 text-center w-20 h-20'>
+            <p className='bg-blue-500 text-white rounded-full px-2'>Feb</p>
+            <p className='text-2xl text-blue-800 font-bold'>21</p>
+            <p>Sab</p>
+          </div>
+          <div className='flex flex-col justify-between'>
+            <p className='text-white text-xl'>Gala Benéfica</p>
+            <p className='bg-blue-200 text-blue-500 py-2 px-3 rounded-full font-bold'>Feb 21 a las 8:00am - 11:00am</p>
+          </div>
+        </div>
+        <FaArrowCircleDown className='text-white text-3xl' />
+        </div>
+      </div>
     </div>
   )
 }
