23082f893025e844bf71cef35cfbd976c2bd89a9
Eduardo282x <eduardokunmixyeah@gmail.com>
2025-06-07 13:49:34 -0400

    remove validation layout

--- src/pages/documents/doc/Doc.tsx
+++ src/pages/documents/doc/Doc.tsx
@@ -9,12 +9,12 @@ export const Doc = () => {
             <HeaderPages title='Documentos' Icon={IoDocumentTextOutline} />
             <div className='bg-[#3779c3] rounded-2xl'>
                 <div className='flex items-center justify-end'>
-                    
+
                 </div>
 
                 <div className='flex flex-wrap items-start justify-start gap-4 w-[80%] mx-auto py-20'>
                     {[1, 2, 3, 4, 5, 6, 7, 8].map(item => (
-                        <CardDocuments key={item}/>
+                        <CardDocuments name={item} key={item} />
                     ))}
                 </div>
 
@@ -23,14 +23,17 @@ export const Doc = () => {
     )
 }
 
+interface CardDocumentsProps {
+    name: number
+}
 
-const CardDocuments = () => {
+const CardDocuments = ({ name }: CardDocumentsProps) => {
     return (
         <div className='bg-[#3779c3] rounded-2xl border border-white w-48 h-40 p-1'>
             <div className='flex items-center justify-between px-2 text-white py-1'>
                 <div className='flex items-center gap-2'>
                     <BsImage />
-                    <p>1.png</p>
+                    <p>{name}.png</p>
                 </div>
 
                 <FaEllipsisVertical />
