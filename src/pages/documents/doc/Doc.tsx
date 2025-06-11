995c635c82cc0c5e14571cef33aa5dc8e6404da2
Lee-gif212 <leefereira2@gmail.com>
2025-06-11 10:55:57 -0400

    Avance en el apartado de documentos

--- src/pages/documents/doc/Doc.tsx
+++ src/pages/documents/doc/Doc.tsx
@@ -1,55 +1,12 @@
-import { HeaderPages } from '@/pages/layout/Header'
-import { BsImage } from 'react-icons/bs'
-import { FaEllipsisVertical } from 'react-icons/fa6'
-import { IoDocumentTextOutline } from 'react-icons/io5'
+import { DocumentosTable } from "./documentos-table"
 
-export const Doc = () => {
-    return (
-        <div>
-            <HeaderPages title='Documentos' Icon={IoDocumentTextOutline} />
-            <div className='bg-[#3779c3] rounded-2xl'>
-                <div className='flex items-center justify-end'>
-
-                </div>
-
-                <div className='flex flex-wrap items-start justify-start gap-4 w-[80%] mx-auto py-20'>
-                    {[1, 2, 3, 4, 5, 6, 7, 8].map(item => (
-                        <CardDocuments name={item} key={item} />
-                    ))}
-                </div>
-
-            </div>
-        </div>
-    )
-}
-
-interface CardDocumentsProps {
-    name: number
-}
-
-const CardDocuments = ({ name }: CardDocumentsProps) => {
-    return (
-        <div className='bg-[#3779c3] rounded-2xl border border-white w-48 h-40 p-1'>
-            <div className='flex items-center justify-between px-2 text-white py-1'>
-                <div className='flex items-center gap-2'>
-                    <BsImage />
-                    <p>{name}.png</p>
-                </div>
-
-                <FaEllipsisVertical />
-            </div>
-
-            <div className='w-full h-[80%] bg-white rounded-2xl'>
-
-            </div>
-        </div>
-    )
-}
-
-const ListDocuments = () => {
-    return (
-        <div>
-
-        </div>
-    )
+export default function Doc() {
+  return (
+    <div className="h-fit">
+      <DocumentosTable />
+    </div>
+          
+        
+      
+  )
 }
