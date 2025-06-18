892001d590e98e333c65c0c90ce230fcb37b8418
Eduardo282x <eduardokunmixyeah@gmail.com>
2025-06-18 11:33:55 -0400

    fix scroll in grid-view documents

--- src/pages/documents/doc/grid-view.tsx
+++ src/pages/documents/doc/grid-view.tsx
@@ -61,7 +61,7 @@ export function GridView({
   }
 
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4">
+    <div className="h-[28rem] overflow-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4">
       {data.map((document) => (
         <div
           key={document.id}
