7dbe669784c4e6f9d7668b6df3ce45989b49a406
Eduardo282x <eduardokunmixyeah@gmail.com>
2025-07-01 00:05:44 -0400

    fix provider service

--- src/services/provider/provider.service.ts
+++ src/services/provider/provider.service.ts
@@ -1,11 +1,10 @@
 import { deleteDataApi, getDataApi, postDataApi, putDataApi } from "@/services/api"
 import { ProviderBody } from "./provider.interface";
-import { getProviders, postProviders, putProviders, deleteProviders } from "@/services/provider/provider.service";
 
 const providersUrl = "/proveedores";
 
 export const getProviders = async () => {
-    return await getDataApi("/proveedores");
+    return await getDataApi(providersUrl);
 }
 export const postProviders = async (data: ProviderBody) => {
     return await postDataApi(providersUrl, data)
